c5fccef7cd9201422dda245f1f6d5c1f7d2221bb
martalt89 <martin.altunyan@gmail.com>
2019-02-18 23:05:22 -0800

    Ready for deployment

--- server.js
+++ server.js
@@ -26,10 +26,12 @@ require('./config/passport')(passport);
 //Use Routes
 app.use('/api/users', users);
 app.use('/api/orders', orders);
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-})
-
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(client / build));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server runing on port ${port}`))

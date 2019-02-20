6c1fea9185fca640c9c22c3c7b3af3f395a35942
martalt89 <martin.altunyan@gmail.com>
2019-02-20 13:55:33 -0800

    Changed

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-    mongoURI: 'mongodb://cloudvibeshookah:cloudvibeshookah2019@ds161134.mlab.com:61134/cloudvibeshookah',
-    secretOrKey: 'secret'
-
-};
+if (process.env.NODE_ENV === 'production') {
+    module.exports = require('./keys_prod');
+} else {
+    module.exports = require('./keys_dev');
+}
--- server.js
+++ server.js
@@ -26,10 +26,14 @@ require('./config/passport')(passport);
 //Use Routes
 app.use('/api/users', users);
 app.use('/api/orders', orders);
+if (process.env.NODE_ENV === 'production') {
 
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-})
+    //Set Static Folder
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 const port = process.env.PORT || 5000;
 
